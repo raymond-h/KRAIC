1a2c51136a867ed9596d66f5d77e00275c817f34
Raymond Hammarling <raykay.rh@gmail.com>
2013-03-22 19:06:42 +0100

    Added file that didn't get included at first

---
+++ Android-IRC-Client/src/se/kayarr/ircclient/views/FadeAwayLinesLayout.java
@@ -0,0 +1,194 @@
+package se.kayarr.ircclient.views;
+
+import se.kayarr.ircclient.irc.output.OutputLine;
+import android.animation.LayoutTransition;
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.os.Build;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+public class FadeAwayLinesLayout extends ViewGroup {
+	public static final String TAG = FadeAwayLinesLayout.class.getSimpleName();
+	
+	private LinearLayout mainList;
+	
+	volatile private long lastViewAddTime = 0;
+	
+	private class TriggerViewCleanupRunnable implements Runnable {
+		
+		private long addTime;
+		
+		public TriggerViewCleanupRunnable(long addTime) {
+			this.addTime = addTime;
+		}
+		
+		public void run() {
+			
+			if(addTime != lastViewAddTime) return;
+			
+			Log.d(TAG, "*** Time to check if there any views outside");
+			
+			triggerRemovingOutsideViews();
+			
+		}
+		
+	}
+	
+	public FadeAwayLinesLayout(Context context) {
+		this(context, null);
+	}
+	
+	@SuppressLint("NewApi")
+	private void setLayoutTransitionForList(LinearLayout list) {
+		
+		if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
+			LayoutTransition trans = new LayoutTransition();
+			
+			trans.addTransitionListener(new LayoutTransition.TransitionListener() {
+				
+				public void startTransition(LayoutTransition transition,
+						ViewGroup container, View view, int transitionType) {
+				}
+				
+				public void endTransition(LayoutTransition transition, ViewGroup container,
+						View view, int transitionType) {
+					
+					//Log.d(TAG, "endTransition triggered with " + transitionType);
+					
+					if(transitionType == LayoutTransition.CHANGE_APPEARING) {
+						
+						//Log.d(TAG, "endTransition triggered with CHANGE_APPEARING");
+						
+						//*
+						lastViewAddTime = System.currentTimeMillis();
+						
+						container.postDelayed(
+								new TriggerViewCleanupRunnable(lastViewAddTime),
+								2000);
+						//*/
+					}
+				}
+			});
+			
+			mainList.setLayoutTransition(trans);
+		}
+	}
+
+	public FadeAwayLinesLayout(Context context, AttributeSet attrs) {
+		super(context, attrs);
+		
+		mainList = new LinearLayout(context);
+		mainList.setLayoutParams(
+				new FadeAwayLinesLayout.LayoutParams(
+						LayoutParams.FILL_PARENT,
+						LayoutParams.WRAP_CONTENT
+				)
+		);
+		mainList.setOrientation(LinearLayout.VERTICAL);
+		setLayoutTransitionForList(mainList);
+		
+		addView(mainList);
+	}
+	
+	public void addLine(OutputLine line) {
+		TextView textLine = new TextView( getContext() );
+		
+		textLine.setLayoutParams(
+				new LinearLayout.LayoutParams(
+						LayoutParams.WRAP_CONTENT,
+						LayoutParams.WRAP_CONTENT
+				)
+		);
+		
+		textLine.setText(line.getOutput());
+		
+		textLine.setTextColor( getContext().getResources().getColor(android.R.color.primary_text_dark) );
+		
+		mainList.addView(textLine);
+	}
+	
+	public void clearLines() {
+		mainList.removeAllViews();
+	}
+	
+	private void triggerRemovingOutsideViews() {
+		
+		for(int i = 0; i < mainList.getChildCount(); i++) {
+			View child = mainList.getChildAt(i);
+			
+			if(child.getTop() > getHeight()) {
+				int removeCount = mainList.getChildCount() - i;
+				
+				Log.d(TAG, "Child " + i + " is outside, remove " + removeCount + " items from beginning");
+				
+				mainList.removeViews(0, removeCount);
+				
+				break;
+			}
+		}
+		
+	}
+	
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+    	
+        measureChildren(
+        		MeasureSpec.makeMeasureSpec( MeasureSpec.getSize(widthMeasureSpec), MeasureSpec.AT_MOST ),
+        		MeasureSpec.makeMeasureSpec( 0, MeasureSpec.UNSPECIFIED )
+        );
+        
+        setMeasuredDimension(
+        		MeasureSpec.getSize(widthMeasureSpec),
+        		MeasureSpec.getSize(heightMeasureSpec)
+        );
+    }
+
+    @Override
+    protected void onLayout(boolean changed, int l, int t,
+            int r, int b) {
+    	
+        int count = getChildCount();
+
+        for (int i = 0; i < count; i++) {
+            View child = getChildAt(i);
+            if (child.getVisibility() != GONE) {
+
+                int childLeft = getPaddingLeft();
+                int childBottom = getMeasuredHeight() - getPaddingBottom();
+                
+                int childTop = childBottom - ( getPaddingTop() + child.getMeasuredHeight() );
+                int childRight = childLeft + child.getMeasuredWidth() - getPaddingRight();
+                
+                child.layout(childLeft, childTop, childRight, childBottom);
+
+            }
+        }
+    }
+    
+    public static int resolveSizeAndStateCompat(int size, int measureSpec, int childMeasuredState) {
+        int result = size;
+        int specMode = MeasureSpec.getMode(measureSpec);
+        int specSize =  MeasureSpec.getSize(measureSpec);
+        switch (specMode) {
+	        case MeasureSpec.UNSPECIFIED:
+	            result = size;
+	            break;
+	        case MeasureSpec.AT_MOST:
+	            if (specSize < size) {
+	                result = specSize | MEASURED_STATE_TOO_SMALL;
+	            } else {
+	                result = size;
+	            }
+	            break;
+	        case MeasureSpec.EXACTLY:
+	            result = specSize;
+	            break;
+        }
+        return result | (childMeasuredState & MEASURED_STATE_MASK);
+    }
+}
